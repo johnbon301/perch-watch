ba94f1284896d7751bbdc0ad27fbb38024bdc352
Jonathan Ferman-Ramirez <jferman80@gmail.com>
2026-08-19 15:50:27 -0700

    comments made and a few things changed

--- Movement.py
+++ Movement.py
@@ -2,8 +2,16 @@ import time
 import cv2
 from ultralytics import YOLO
 
-# fine-tunned model goes here (nano is used temp. for testing)
+# fine-tunned models goes here
+""" Add explaniation to README to obtain model for script """
+
+speciesOnlyModel = YOLO('./models/speciesOnly/birdDetectionUmatilla_ncnn_model')
+birdOnlyModel = YOLO('./models/birdOnly/bird_detection(best_weight)v2_ncnn_model')
 model = YOLO('yolo11n.pt')
+
+# put species in a list which is captured on the species model metadata
+DETER_CLASS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 
 # fps stuff
 pTime = 0
@@ -15,10 +23,15 @@ cap = cv2.VideoCapture(0)  # creates a capture object that opens up the default 
 if not cap.isOpened():
     raise RuntimeError("Camera failed to open")
 
-
-cap.set(3, 640) # sets the width and height of the window size
+# sets the height and width of the window size
+cap.set(3, 640) 
 cap.set(4, 480)
 
+
+""" Two models are needed for this script to work. The stage 1 part will only detect for bird and nothing else.
+    Need to make sure endangered species are on this part too. The second stage will crop the bounded box for the 
+    second model to detect which species it is looking at. This will log the certain species
+"""
 while True:  # infinite loop
     success, frame = cap.read()  # returns to values that successfully captures the frame (always have)
     if not success:
@@ -26,7 +39,14 @@ while True:  # infinite loop
 
     # load and detect
     results = model(frame, conf=0.5)
+    results = birdOnlyModel(frame, conf=0.5, classes=[0] ,stream=True)
 
+    """
+    for every result from model, extract every box and its point, calculate mid point, convert those mid points to an angle where 
+    the servos can move. add edges cases for when the coorindates push the turrent beyond the angles. Make sure that every second
+    or so, there is a sleep timer to prevent continous firing. Control preassure to ensure it does not harm bird either. Probably 
+    a sleep timer where after the first second. Make sure if a bird is endanger to avoid completely
+    """
     # extracts the points and converts them to the middle of the box
     for box in results.boxes[0]:
         x1, y1, x2, y2 = box.xyxy
@@ -44,11 +64,10 @@ while True:  # infinite loop
             (255, 0, 255), 2) # adds and edits the window for "Image"
     
     cv2.imshow("Result", annotatedFrame)
-    # Wait 1ms for the next frame, or quit if 'q' is pressed
+    # wait 1ms for the next frame, or quit if 'q' is pressed
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Clean up properly outside the loop
+# clean up properly outside the loop
 cap.release()
 cv2.destroyAllWindows()
-
---
+++ turretMovement.py
@@ -0,0 +1,29 @@
+from adafruit_servokit import ServoKit
+from gpiozero import DigitalOutputDevice
+from time import sleep
+
+
+# class Turret:
+#     def __init__(self, angle):
+#         self.panAngle = panAngle
+#         self.tiltAngle = tiltAngle
+#         self.kit = ServoKit(channels=16)
+
+#     def moveTurret(self):
+#         kit.servo[0].angle = self.angle # based on middle box coordinate of ai model
+#         kit.servo[1].angle = self.angle # MAX ANGLE(up and down) 80 - 180
+
+
+
+kit = ServoKit(channels=16)
+trigger = DigitalOutputDevice(17)  # your GPIO pin
+
+# Servo check
+kit.servo[0].angle = 90
+kit.servo[1].angle = 90
+sleep(5)
+
+# Pump check
+trigger.on()
+sleep(1.5)
+trigger.off()
